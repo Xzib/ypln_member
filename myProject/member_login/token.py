de72d0d34f6c9393c8649c5fbc6f4891fa3858bc
Xzib <syedzohaibali94@gmail.com>
2020-01-04 17:43:09 +0500

    testing code for email config

--- myProject/member_login/token.py
+++ myProject/member_login/token.py
@@ -1,33 +1,34 @@
 from flask import Flask
-from flask_mail import Mail, Message
+from flask_mail import Mail,Message
 import os
 
+mail = Mail()
 app = Flask(__name__)
 
-###### Form configuration ######
-app.config['SECRET_KEY']= 'mysecretkey'
+# ###### Form configuration ######
+# app.config['SECRET_KEY']= 'mysecretkey'
 
-##### Database configuration #####
-basedir = os.path.abspath(os.path.dirname(__file__))
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'+os.path.join(basedir,'data.sqlite')
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# ##### Database configuration #####
+# basedir = os.path.abspath(os.path.dirname(__file__))
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'+os.path.join(basedir,'data.sqlite')
+# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 #### Email Configuration #####
 app.config['MAIL_SERVER'] = 'mail.yplnetwork.com '
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = False
 app.config['MAIL_USE_SSL'] = True
-#app.config['MAIL_DEBUG'] = True
-app.config['MAIL_USERNAME'] = 'member@yplnetwork.com'
-app.config['MAIL_PASSWORD'] = '4772b4ah6z15'
+app.config['MAIL_DEBUG'] = True
+app.config['MAIL_USERNAME'] = 'test@yplnetwork.com'
+app.config['MAIL_PASSWORD'] = '1234@1234'
 app.config['DEFAULT_MAIL_SENDER'] = None 
 
-mail = Mail(app)
+mail.init_app(app)
 
 if __name__ == "__main__":
-
-    msg = Message(subject = 'HELLO',
-                    sender = app.config.get('MAIL_USERNAME'),
-                    recipients=["syedzohaibali94@hotmail.com"],
-                    body = 'test message')
-    mail.send(msg)               
+    with app.app_context():
+        msg = Message(subject = 'HELLO',
+                        sender = 'test@yplnetwork.com',
+                        recipients=['syedzohaibali94@hotmail.com'],
+                        body = 'test message')
+        mail.send(msg)               
